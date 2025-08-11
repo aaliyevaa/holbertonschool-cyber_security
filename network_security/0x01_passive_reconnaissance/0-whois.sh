5e4c965646c8a10ebd3b1092189757d3f179deb8
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-08-11 16:59:45 +0400

    Edit Task 0

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/^(Registrant|Admin|Tech)/ {sub(/^[ \t]+/, ""); gsub(/: /, ","); gsub(/[ \t]+/, " "); sub(/,$/, ""); print}' > $1.csv
+sudo whois $1 | awk 'BEGIN { FS=": "; OFS="," } /^(Registrant|Admin|Tech)/ {print $1, $2}' > $1.csv 
