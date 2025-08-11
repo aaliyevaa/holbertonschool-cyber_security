5c2ab4a1fe7c2f0e8ec3fc64c1f910ce88381a81
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-08-11 18:14:16 +0400

    edit 0

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/^(Registrant|Admin|Tech)/ {gsub(/^[[:space:]]+|[[:space:]]+$/,"",$0); split($0,a,":"); gsub(/^ /,"",a[2]); gsub(/ /,"$",a[2]); print a[1]","a[2]}' 
+whois $1 | awk '/^(Registrant|Admin|Tech)/ {gsub(/^[[:space:]]+|[[:space:]]+$/,"",$0); split($0,a,":"); gsub(/^ /,"",a[2]); gsub(/ /,"$",a[2]); print a[1]","a[2]}' > $1.csv
